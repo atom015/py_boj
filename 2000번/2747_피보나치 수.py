97e935179797e6dde413411414b6cdab358f4072
zeezlelove <zeezlelove@gmail.com>
2019-04-10 21:30:27 +0900

    백준 문제풀이

--- 2000번/2747_피보나치 수.py
+++ 2000번/2747_피보나치 수.py
@@ -1,7 +1,9 @@
+def FIB(n):
+    cnt = 1
+    fib = [0, 1]
+    for i in range(fib[1], n):
+        sum = fib[i-1] + fib[i]
+        fib.append(sum)
+    return fib[len(fib)-1]
 n = int(input())
-cnt = 1
-fib = [0, 1]
-for i in range(fib[1], n):
-    sum = fib[i-1] + fib[i]
-    fib.append(sum)
-print(fib[len(fib)-1])
+print(FIB(n))
