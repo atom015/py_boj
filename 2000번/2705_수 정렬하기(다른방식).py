1c360ebb20d75fb5a68de5331cd871a22efd68d4
zeezlelove <zeezlelove@gmail.com>
2019-04-21 23:50:54 +0900

    백준 문제풀이

--- 2000번/2705_수 정렬하기(다른방식).py
+++ 2000번/2705_수 정렬하기(다른방식).py
@@ -4,11 +4,8 @@ for i in range(int(input())):
 
 for i in range(len(n)):
     for j in range(len(n)):
-        tmp_i = n[i]
-        tmp_j = n[j]
         if i != j:
             if n[i] < n[j]:
-                n[i] = tmp_j
-                n[j] = tmp_i
+                n[i],n[j] = n[j],n[i]
 for i in n:
     print(i)
