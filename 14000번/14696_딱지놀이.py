2b29f53108908f2d696d39b75fdcca4d2222db21
zeezlelove <zeezlelove@gmail.com>
2019-04-23 22:23:44 +0900

    백준 문제풀이

---
+++ 14000번/14696_딱지놀이.py
@@ -0,0 +1,26 @@
+for i in range(int(input())):
+    a = list(input().split())
+    b = list(input().split())
+    del a[0],b[0]
+    if a.count("4") < b.count("4"):
+        print("B")
+    elif a.count("4") > b.count("4"):
+        print("A")
+    else:
+        if a.count("3") < b.count("3"):
+            print("B")
+        elif a.count('3') > b.count('3'):
+            print("A")
+        else:
+            if a.count('2') < b.count('2'):
+                print("B")
+            elif a.count('2') > b.count('2'):
+                print("A")
+            else:
+                if a.count('1') < b.count('1'):
+                    print("B")
+                elif a.count('1') > b.count('1'):
+                    print("A")
+                elif a.count('1') == b.count('1'):
+                    print("D")
+                        
