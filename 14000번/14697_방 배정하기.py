20c76bf2b8f00b1b2976fb22c219df8907427d21
zeezlelove <zeezlelove@gmail.com>
2019-04-29 17:50:40 +0900

    백준 문제풀이

--- 14000번/14697_방 배정하기.py
+++ 14000번/14697_방 배정하기.py
@@ -3,8 +3,9 @@ a,b,c,d = map(int,input().split())
 for i in range(50):
     for ii in range(50):
         for j in range(50):
-            if (i*a+ii*b+j*c) == d:
+            if (i*a+ii*b+j*c) == d: #(i*a(방의정원))+(ii*b(방의정원))+(j*c(방의정원)) == d(전체 학생수)
                 print(1)
                 sys.exit()
 print(0)
 sys.exit()
+#브루트포스
