3faee823e54a951fb0e53022a3b217a49a2c2fff
zeezlelove <zeezlelove@gmail.com>
2019-05-13 23:09:31 +0900

    백준 문제풀이

---
+++ 2000번/2309_일곱 난쟁이.py
@@ -0,0 +1,14 @@
+def main(li,s):
+    for i in range(9):
+        for j in range(9):
+            if i != j:
+                if 100 == s-(li[i]+li[j]):
+                    for k in sorted(li):
+                        if k != li[i] and k != li[j]:
+                            print(k)
+                    return                         
+num_li = []
+for i in range(9):
+    num_li.append(int(input()))
+main(num_li,sum(num_li))
+#이문제는 백설공주와 일곱난쟁이랑 비슷한 브루트 포스문제이다.
