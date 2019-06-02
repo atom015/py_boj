48f06d916e23389f56f1f72adb515b53fb37f9c9
zeezlelove <zeezlelove@gmail.com>
2019-06-02 17:37:48 +0900

    백준 문제풀이

--- 1000번/1076_저항.py
+++ 1000번/1076_저항.py
@@ -3,10 +3,6 @@ mul = [1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000]
 li = []
 for i in range(3):
     n = input()
-    if i == 0:
-        li.append(n)
-    elif i == 1:
-        li.append(n)
-        result = re[li[0]]+re[li[1]]
-    elif i == 2:
-        print(int(result)*int(mul[int(re[n])]))
+    if i == 2:
+        print(int(re[li[0]]+re[li[1]])*int(mul[int(re[n])]))
+    li.append(n)
