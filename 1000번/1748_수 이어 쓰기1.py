ad03209f80394011b16ad5616ffdc916788d08e7
zeezlelove <zeezlelove@gmail.com>
2019-06-26 22:20:31 +0900

    백준 문제풀이

--- 1000번/1748_수 이어 쓰기1.py
+++ 1000번/1748_수 이어 쓰기1.py
@@ -1,8 +1,6 @@
-def main(n):
-    result,i = 0,1
-    while i <= n:
-        result += (n-i+1)
-        i *= 10
-    return result
-        
-print(main(int(input())))
+n = int(input())
+result,i = 0,1
+while i <= n:
+    result += (n-i+1)
+    i *= 10
+print(result)
