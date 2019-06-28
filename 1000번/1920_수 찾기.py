4c0c5945dd94c70c0f559454bbbfb4cccbef5e52
zeezlelove <zeezlelove@gmail.com>
2019-06-28 21:25:44 +0900

    백준 문제풀이

--- 1000번/1920_수 찾기.py
+++ 1000번/1920_수 찾기.py
@@ -1,11 +1,24 @@
-import sys
-n = int(sys.stdin.readline())
-nli = list(map(int,sys.stdin.readline().split()))
-m = int(sys.stdin.readline())
-mli = list(map(int,sys.stdin.readline().split()))
-for i in mli:
-    if i in nli:
-        print(1)
+n = int(input())
+nli = sorted(list(map(int,input().split()))) #리스트 정렬
+m = int(input())
+mli = list(map(int,input().split()))
+def binary_search_recursion(target,low,high,data):
+    if low > high: #만약에 시작지점이 끝지점보다 크면
+        return 0 #None return
+    mid = (low+high)//2
+    if data[mid] == target:
+        return 1
+    elif data[mid] > target:
+        high = mid - 1
     else:
-        print(0)
-#원래 이분탐색으로 해야하는데 일부로 sys.stdin.readline()애 pypy3으로 해서 시간초과를 없앴다.
+        low = mid + 1
+    return binary_search_recursion(target,low,high,data)
+for i in mli:
+    print(binary_search_recursion(i,0,n-1,nli))
+"""
+target : 찾고자 하는 값
+data : 오름차순으로 정렬된 list
+start : data 의 처음 값 인덱스
+end : data 의 마지막 값 인덱스
+mid : start, end 의 중간 인덱스
+"""
