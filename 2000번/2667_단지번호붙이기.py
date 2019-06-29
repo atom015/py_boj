06495f5be6b97dd59f9b853aad1b8489f4e0cbdd
zeezlelove <zeezlelove@gmail.com>
2019-06-29 17:26:07 +0900

    백준 문제풀이

--- 2000번/2667_단지번호붙이기.py
+++ 2000번/2667_단지번호붙이기.py
@@ -5,19 +5,19 @@ dy = [0,1,0,-1]
 result = []
 def dfs(x,y):
     global cnt
+    cnt += 1
     li[x][y] = '0'
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if nx < n and nx >= 0 and ny < n and ny >= 0 and li[nx][ny] == '1':
-            cnt += 1
             dfs(nx,ny)
     return cnt
 
 for i in range(n):
     for j in range(n):
         if li[i][j] == '1':
-            cnt = 1
+            cnt = 0
             result.append(dfs(i,j))
 print(len(result))
 for i in sorted(result):
