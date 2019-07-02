9d38d4d0dbf6d009198ff322d10beccfaf5bb8df
zeezlelove <zeezlelove@gmail.com>
2019-07-02 21:19:57 +0900

    백준 문제풀이

--- 1000번/1940_주몽(pypy3).py
+++ 1000번/1940_주몽(pypy3).py
@@ -2,12 +2,8 @@ n = int(input())
 m = int(input())
 li = list(map(int,input().split()))
 cnt = 0
-chk = []
 for i in range(n):
-    for j in range(n):
-        if i != j:
-            if li[i]+li[j] == m and li[i] not in chk and li[j] not in chk:
-                chk.append(li[i])
-                chk.append(li[j])
-                cnt += 1
+    for j in range(i+1,n):
+        if (li[i] + li[j]) == m:
+            cnt += 1
 print(cnt)
