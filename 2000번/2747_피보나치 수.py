be00133f166f813034b5f566c9027eef437b259f
zeezlelove <zeezlelove@gmail.com>
2019-07-03 21:58:33 +0900

    백준 문제풀이

--- 2000번/2747_피보나치 수.py
+++ 2000번/2747_피보나치 수.py
@@ -1,7 +1,14 @@
 n = int(input())
+dp = [0 for i in range(n+1)]
+dp[0] = 0
+dp[1] = 1
 def fibo(n):
     if n <= 1:
-        return 1
+        return dp[n]
     else:
-        return fibo(n-1) + fibo(n-2)
+        if dp[n] > 0:
+            return dp[n]
+        else:
+            dp[n] = fibo(n-1) + fibo(n-2)
+            return dp[n]
 print(fibo(n))
