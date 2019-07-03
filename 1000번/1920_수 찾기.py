1f4cf673e9890d1b65c4cc6b1d6390b3a809d4e8
zeezlelove <zeezlelove@gmail.com>
2019-07-03 20:47:43 +0900

    백준 문제풀이

--- 1000번/1920_수 찾기.py
+++ 1000번/1920_수 찾기.py
@@ -1,24 +1,17 @@
 n = int(input())
-nli = sorted(list(map(int,input().split()))) #리스트 정렬
+nl = sorted(list(map(int,input().split())))
 m = int(input())
-mli = list(map(int,input().split()))
-def binary_search_recursion(target,low,high,data):
-    if low > high: #만약에 시작지점이 끝지점보다 크면
-        return 0 #None return
-    mid = (low+high)//2
-    if data[mid] == target:
+ml = list(map(int,input().split()))
+def binary_search(n,lo,hi,li):
+    if lo > hi:
+        return 0
+    mid = (lo+hi) // 2
+    if li[mid] == n:
         return 1
-    elif data[mid] > target:
-        high = mid - 1
+    if li[mid] < n:
+        lo = mid + 1
     else:
-        low = mid + 1
-    return binary_search_recursion(target,low,high,data)
-for i in mli:
-    print(binary_search_recursion(i,0,n-1,nli))
-"""
-target : 찾고자 하는 값
-data : 오름차순으로 정렬된 list
-start : data 의 처음 값 인덱스
-end : data 의 마지막 값 인덱스
-mid : start, end 의 중간 인덱스
-"""
+        hi = mid - 1
+    return binary_search(n,lo,hi,li)
+for i in ml:
+    print(binary_search(i,0,n-1,nl))
