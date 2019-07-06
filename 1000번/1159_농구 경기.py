ddc9f96b37acf715db7f9c52594165e4fcfa2774
zeezlelove <zeezlelove@gmail.com>
2019-07-06 22:14:02 +0900

    백준 문제풀이

--- 1000번/1159_농구 경기.py
+++ 1000번/1159_농구 경기.py
@@ -1,20 +1,14 @@
-n = int(input())
-li = [input() for i in range(n)]
-result = []
-chk = {}
+li = [input() for i in range(int(input()))]
+overlap = set(i[0] for i in li)
+chk = {i:0 for i in overlap}
 for i in li:
-    if i[0] not in chk:
-        chk[i[0]] = 1
-for i in range(n):
-    for j in range(n):
-        if i != j:
-            if li[j] != None and li[i] != None and li[i][0] == li[j][0]:
-                chk[li[i][0]] += 1
-                li[j] = None
+    chk[i[0]] += 1
+result = []
 for k,v in chk.items():
     if v >= 5:
         result.append(k)
 if len(result) == 0:
     print("PREDAJA")
 else:
-    for i in sorted(result):print(i,end='')
+    for i in sorted(result):
+        print(i,end='')
