ca0831d984103a43cad6c3fa9e0d51434bce8587
zeezlelove <zeezlelove@gmail.com>
2019-07-07 19:54:41 +0900

    백준 문제풀이

--- 1000번/1546_평균.py
+++ 1000번/1546_평균.py
@@ -1,4 +1,4 @@
-n = int(input())
-li = list(map(int,input().split()))
-s = [i/max(li)*100 for i in li]
-print(sum(s)/len(li))
+n = int(input()) #과목의 개수
+li = list(map(int,input().split())) #현재성적
+s = [i/max(li)*100 for i in li] #고친성적으로 점수/m(최대값)*100으로 고쳐준다.
+print(sum(s)/len(li)) #모든점수/과목의개수로 평균을 구해서 출력해준다.
