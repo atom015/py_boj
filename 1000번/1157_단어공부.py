a783dad2703b355453c7f0466f74a6a766735044
zeezlelove <zeezlelove@gmail.com>
2019-07-07 14:57:18 +0900

    백준 문제풀이

--- 1000번/1157_단어공부.py
+++ 1000번/1157_단어공부.py
@@ -1,15 +1,10 @@
-al_dic = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0,
-'O':0,'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
-n = list(input().upper())
-
-result = []
+n = input().upper()
+chk = {i:0 for i in set(n)}
 for i in n:
-    al_dic[i] += 1
-
-for key, value in al_dic.items():
-    if value == max(al_dic.values()):
-        result.append(key)
-if len(result) >= 2:
-    print('?')
+    chk[i] += 1
+max = max(chk.values())
+result = set(k for k,v in chk.items() if max == v)
+if len(result) == 1:
+    print(result.pop())
 else:
-    print(result[0])
+    print("?")
