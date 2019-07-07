955babe9d00f229f021de33d111b64825c2f33b3
zeezlelove <zeezlelove@gmail.com>
2019-07-07 16:37:11 +0900

    백준 문제풀이

--- 1000번/1110_더하기사이클.py
+++ 1000번/1110_더하기사이클.py
@@ -1,11 +1,12 @@
-cnt = 0
-n = input()
-c = [int(i) for i in n]
-c = str(c[-1])+str(sum(c))[-1]
+cnt = 0 #사이클의 길이체크
+n = input() #ex)26
+c = [int(i) for i in n] #입력받은수를 정수리스트로 나눈다 ,ex)[2,6]
+c = str(c[-1])+str(sum(c))[-1] #ex)"6"+"8"
+#이런식으로 다시 원래수로 돌아올때까지 반복한다.
 while 1:
     cnt += 1
     if int(c) == int(n):
         break
     c = [int(i) for i in c]
     c = str(c[-1])+str(sum(c))[-1]
-print(cnt)
+print(cnt) #사이클길이 출력
--- 1000번/1157_단어공부.py
+++ 1000번/1157_단어공부.py
@@ -1,4 +1,4 @@
-n = input().upper()
+n = input().upper() #대소문자는 구분하지않으니까 대문자로 바꾸어주었다.
 chk = {i:0 for i in set(n)}
 for i in n:
     chk[i] += 1
