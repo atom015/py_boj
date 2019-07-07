f3af87810598bb978402990290c32111c5c96cd8
zeezlelove <zeezlelove@gmail.com>
2019-07-07 20:25:14 +0900

    백준 문제풀이

--- 1000번/1924_2007년.py
+++ 1000번/1924_2007년.py
@@ -2,6 +2,6 @@ import calendar
 day = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
 a,b=map(int,input().split())
 
-k = calendar.weekday(2007,a,b)
+k = calendar.weekday(2007,a,b) #이함수는 월요일은 0 화요일은 1로 나타내주니까 day에 k인덱스를 출력해준다.
 
 print(day[k])
