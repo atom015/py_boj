21f0755f90395d363e73f1d342071944da329d33
zeezlelove <zeezlelove@gmail.com>
2019-07-07 15:04:57 +0900

    백준 문제풀이

--- 1000번/1427_소트인사이드.py
+++ 1000번/1427_소트인사이드.py
@@ -1,10 +1,3 @@
-n = input()
-arr = []
-for i in range(len(n)):
-    arr += n[i]
-    arr = [int(i) for i in arr]
-    arr.sort()
-    arr.reverse()
-
-for i in range(len(arr)):
-    print(arr[i],end='')
+n = sorted(list(map(int,input())))
+for i in reversed(n):
+    print(i,end='')
