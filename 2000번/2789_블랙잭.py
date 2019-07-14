b2be526b0668d804e76490e38a398d5fe14e9bad
zeezlelove <zeezlelove@gmail.com>
2019-07-15 00:45:50 +0900

    백준 문제풀이

--- 2000번/2789_블랙잭.py
+++ 2000번/2789_블랙잭.py
@@ -1,10 +1,13 @@
-ans = 0
+compare = 0
 n,m = map(int,input().split())
 li = list(map(int,input().split()))
 for i in range(0,n-2):
     for j in range(i+1,n-1):
         for k in range(j+1,n):
             sum = li[i]+li[j]+li[k]
-            if sum <= m and m-sum < m - ans:
-                ans = sum
-print(ans)
+            if sum == m:
+                print(sum)
+                exit()
+            elif compare < sum < m:
+                compare = sum
+print(compare)
