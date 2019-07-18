aecfd576aa645f818e781dfaa2fab6d522e1e735
zeezlelove <zeezlelove@gmail.com>
2019-07-19 01:08:39 +0900

    백준 문제풀이

---
+++ 4000번/4150_피보나치 수.py
@@ -0,0 +1,16 @@
+import sys
+sys.setrecursionlimit(50000)
+n = int(input())
+dp = [0 for i in range(n+1)]
+dp[0] = 0
+dp[1] = 1
+def fibo(n):
+    if n <= 1:
+        return dp[n]
+    else:
+        if dp[n] > 0:
+            return dp[n]
+        else:
+            dp[n] = fibo(n-1)+fibo(n-2)
+            return dp[n]
+print(fibo(n))
