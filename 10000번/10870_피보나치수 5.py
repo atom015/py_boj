406b0e78d28fe6d6570fcbd86c8ebba5886b579d
zeezlelove <zeezlelove@gmail.com>
2019-08-01 16:20:49 +0900

    백준 문제풀이

---
+++ 10000번/10870_피보나치수 5.py
@@ -0,0 +1,12 @@
+n = int(input())
+memo = [0 for i in range(n+1)]
+def fibo(n):
+    if n <= 1:
+        memo[n] = n
+        return memo[n]
+    else:
+        if memo[n] > 0:
+            return memo[n]
+        memo[n] = fibo(n-1)+fibo(n-2)
+        return memo[n]
+print(fibo(n))
