b49571356251f469b058a5673a4dcc03a8912ee2
zeezlelove <zeezlelove@gmail.com>
2019-09-03 20:56:51 +0900

    백준 문제풀이

--- 9000번/9095_1,2,3 더하기.py
+++ 9000번/9095_1,2,3 더하기.py
@@ -1,6 +1,6 @@
 t = int(input())
 for i in range(t):
-    dp = [0 for i in range(15)]
+    dp = [0 for i in range(14)]
     dp[0],dp[1],dp[2] = 1,1,2
     k = int(input())
     for i in range(3,k+1):
