57432bcd00063feb271d1965e6c15f7b3b89f8cc
zeezlelove <zeezlelove@gmail.com>
2019-09-06 21:47:16 +0900

    백준 문제풀이

---
+++ 2000번/2579_계단오르기.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = [int(input()) for i in range(n)]
+dp = [0 for i in range(301)]
+dp[0] = a[0]
+dp[1] = max(a[0]+a[1],a[1])
+dp[2] = max(a[0]+a[2],a[1]+a[2])
+for i in range(2,n):
+    dp[i] = max(dp[i-2]+a[i],a[i-1]+a[i]+dp[i-3])
+print(dp[n-1])
