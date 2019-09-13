526aac720c7a585b6c010c24fd718078c6f20eb5
zeezlelove <zeezlelove@gmail.com>
2019-09-13 13:37:00 +0900

    백준 문제풀이

---
+++ 1000번/1373_2진수 8진수.py
@@ -0,0 +1,10 @@
+n = input()
+two = int(n,2)
+print(format(two,'o'))
+# 2진수 -> bin( 정수 )
+# 8진수 -> oct( 정수 )
+# 16진수 -> hex( 정수 )
+#print hex(num) # 10진수 -> 16진수 변환 : 0x3039
+#print int(bin(num),2) # 2진수 -> 10진수 변환 : 12345
+#print int(hex(num),16) # 16진수 -> 10진수 변환 : 12345
+#format(10진수, 바꿀 진법(?))
