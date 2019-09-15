e7a4af67e2f035b3c353c2109f06579afa583543
zeezlelove <zeezlelove@gmail.com>
2019-09-15 17:24:36 +0900

    백준 문제풀이

--- 15000번/15649_N과 M(1)(pypy3).py
+++ 15000번/15649_N과 M(1)(pypy3).py
@@ -60,18 +60,11 @@
 4 3 1 2
 4 3 2 1
 """
-arr = [0 for i in range(9)]
-visited = [False for i in range(9)]
+import itertools
 n,m = map(int,input().split())
-def func(cnt):
-    if cnt == m:
-        for i in range(m):
-            print(arr[i],end=' ')
-        print()
-    for i in range(1,n+1):
-        if visited[i] == False:
-            visited[i] = True
-            arr[cnt] = i
-            func(cnt+1)
-            visited[i] = False
-func(0)
+a = [i for i in range(1,n+1)]
+l = list(itertools.permutations(a,m))
+for i in sorted(l):
+    for j in i:
+        print(j,end=' ')
+    print()
