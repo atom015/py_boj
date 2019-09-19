0cde986b65df4b52302ee304da0edd1a9b84d79b
zeezlelove <zeezlelove@gmail.com>
2019-09-19 19:28:00 +0900

    백준 문제풀이

--- 2000번/2909_캔디 구매.py
+++ 2000번/2909_캔디 구매.py
@@ -18,7 +18,7 @@
 """
 c,k = map(int,input().split())
 tmp = 10**k
-ans = c // tmp*tmp # 184 / 10*10 = 180
-if c % tmp >= tmp//2: # 184 % 10 >= 10//2,4 >= 5
-    ans += tmp
+ans = c // tmp*tmp
+if c % tmp >= tmp//2: #만약에 반올림을 해야하면
+    ans += tmp #k번째의값에 +1해준다.
 print(ans)
