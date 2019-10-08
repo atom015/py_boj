c56de9e89d48105982347a118bdee49bd5259c9d
zeezlelove <zeezlelove@gmail.com>
2019-10-09 00:45:36 +0900

    백준 문제풀이

--- 1000번/1717_집합의 표현.py
+++ 1000번/1717_집합의 표현.py
@@ -1,35 +1,32 @@
 import sys
 ip = sys.stdin.readline
 #재귀함수를 통해 부모노드를 찾고 부모노드를 갱신한다.
-def getParent(p,x):
+def getP(p,x):
     if p[x] == x:
         return x
-    p[x] = getParent(p,p[x])
+    p[x] = getP(p,p[x])
     return p[x]
 #두노드를 비교하고 더작은 부모노드를 두개의노드의 부모노드로 만든다.
-def unionParent(p,a,b):
-    a = getParent(p,a)
-    b = getParent(p,b)
+def union(p,a,b):
+    a = getP(p,a)
+    b = getP(p,b)
     if a < b:
         p[b] = a
     else:
         p[a] = b
 #두 원소가 같은 집합에 포함되어 있는지를 확인한다.
-def findParent(p,a,b):
-    a = getParent(p,a)
-    b = getParent(p,b)
+def findP(p,a,b):
+    a = getP(p,a)
+    b = getP(p,b)
+    #두 부모노드가 같으면 YES출력 아니면 NO출력
     if a == b:
-        return 1
-    return 0
+        return "YES"
+    return "NO"
 n,m = map(int,ip().split())
-p = [i for i in range(1,n+1)]
-p.insert(0,0)
+p = [i for i in range(n+1)]
 for i in range(m):
     a,b,c = map(int,ip().split())
     if a == 0:
-        unionParent(p,b,c)
+        union(p,b,c)
     else:
-        if findParent(p,b,c) == 1:
-            print("YES")
-        else:
-            print("NO")
+        print(findP(p,b,c))
