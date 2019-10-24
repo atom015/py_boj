a6574ff2d95bb00f033e8e93af4bf2848f360b35
zeezlelove <zeezlelove@gmail.com>
2019-10-24 22:29:55 +0900

    백준 문제풀이

--- 10000번/10825_국영수.py
+++ 10000번/10825_국영수.py
@@ -2,6 +2,6 @@ a = []
 for i in range(int(input())):
     s = input().split()
     a.append((int(s[1]),int(s[2]),int(s[3]),s[0]))
-stu.sort(key=lambda x: (-x[0],x[1],-x[2],x[3]))
+a.sort(key=lambda x: (-x[0],x[1],-x[2],x[3]))
 for i in a:
     print(i[3])
