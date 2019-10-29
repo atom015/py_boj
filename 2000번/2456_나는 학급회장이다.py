8e76983d78fef2970928463712b7f76797483f55
zeezlelove <zeezlelove@gmail.com>
2019-10-29 23:37:45 +0900

    백준 문제풀이

--- 2000번/2456_나는 학급회장이다.py
+++ 2000번/2456_나는 학급회장이다.py
@@ -8,7 +8,7 @@ for i in range(int(input())):
     arr[1][0] += b
     arr[2][0] += c
 arr.sort(key=lambda t:(-t[0],-t[1],-t[2],-t[3]))
-if arr[0][0:4] == arr[1][0:4] or arr[0][0:4] == arr[1][0:4] == arr[2][0:4]:
+if arr[0][0:4] == arr[1][0:4]:
     print(0,arr[0][0])
 else:
     print(arr[0][4],arr[0][0])
