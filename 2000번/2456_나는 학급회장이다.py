4f7e5e0c19a8004488c838f9117d5fcfa4c8f354
zeezlelove <zeezlelove@gmail.com>
2019-10-29 23:37:02 +0900

    백준 문제풀이

---
+++ 2000번/2456_나는 학급회장이다.py
@@ -0,0 +1,14 @@
+arr = [[0,0,0,0,1],[0,0,0,0,2],[0,0,0,0,3]]
+for i in range(int(input())):
+    a,b,c = map(int,input().split())
+    arr[0][a] += 1
+    arr[1][b] += 1
+    arr[2][c] += 1
+    arr[0][0] += a
+    arr[1][0] += b
+    arr[2][0] += c
+arr.sort(key=lambda t:(-t[0],-t[1],-t[2],-t[3]))
+if arr[0][0:4] == arr[1][0:4] or arr[0][0:4] == arr[1][0:4] == arr[2][0:4]:
+    print(0,arr[0][0])
+else:
+    print(arr[0][4],arr[0][0])
