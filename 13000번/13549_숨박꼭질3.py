264e10cf0779afcb2214d3ae26d96091d7b99d2a
zeezlelove <zeezlelove@gmail.com>
2019-11-10 22:32:25 +0900

    백준 문제풀이

---
+++ 13000번/13549_숨박꼭질3.py
@@ -0,0 +1,18 @@
+n,k = map(int,input().split())
+q = [[n,0]]
+v = [False for i in range(100001)]
+v[n] = True
+while q:
+    p,cnt = q.pop(0)
+    if p == k:
+        print(cnt)
+        break
+    if  p*2 <= 100000 and v[p*2] == False:
+        q.append([p*2,cnt])
+        v[p*2] = True
+    if 0 <= p-1 and v[p-1] == False:
+        q.append([p-1,cnt+1])
+        v[p-1] = True
+    if  p+1 <= 100000 and v[p+1] == False:
+        q.append([p+1,cnt+1])
+        v[p+1] = True
