becc8ab48135347be82e99622ad894100b5769d3
zeezlelove <zeezlelove@gmail.com>
2019-11-10 22:32:35 +0900

    백준 문제풀이

--- 2000번/2563_색종이.py
+++ 2000번/2563_색종이.py
@@ -2,13 +2,9 @@ board = [[0 for _ in range(101)] for _ in range(101)]
 cnt = 0
 for _ in range(int(input())):
     x,y = map(int,input().split())
-    i = y+1
-    while i <= y+10:
-        j = x+1
-        while j <= x+10:
+    for i in range(y+1,y+11):
+        for j in range(x+1,x+11):
             board[i][j] = 1
-            j += 1
-        i += 1
 for i in range(101):
     for j in range(101):
         if board[i][j] == 1:
