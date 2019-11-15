d9dfe9973a022adb3f1424fa95b09d6e45e0c791
zeezlelove <zeezlelove@gmail.com>
2019-11-15 23:32:20 +0900

    백준 문제풀이

---
+++ 13000번/13301_타일 장식물.py
@@ -0,0 +1,9 @@
+n = int(input())
+if n == 1:
+    print(4)
+    exit()
+dp = [0 for i in range(n)]
+dp[0],dp[1] = 1,1
+for i in range(2,n):
+    dp[i] = dp[i-1]+dp[i-2]
+print((max(dp)+(max(dp)+dp[-2]))*2)
