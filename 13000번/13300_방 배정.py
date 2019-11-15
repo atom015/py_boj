7eccf91babf97d2f8beb72b7036d3a6432f23d19
zeezlelove <zeezlelove@gmail.com>
2019-11-15 23:00:36 +0900

    백준 문제풀이

--- 13000번/13300_방 배정.py
+++ 13000번/13300_방 배정.py
@@ -1,11 +1,11 @@
-n,k = map(int,input().split())
+n, k = map(int, input().split())
 room = 0
 cls = [[] for i in range(6)]
 for i in range(n):
-    s,y = map(int,input().split())
-    cls[y-1].append(s)
+    s, y = map(int, input().split())
+    cls[y - 1].append(s)
 for v in cls:
-    girl,boy = 0,0
+    girl, boy = 0, 0
     for i in v:
         if girl == k:
             room += 1
@@ -15,12 +15,9 @@ for v in cls:
             room += 1
             boy = 0
 
-        if i == 0:
-            girl += 1
-        elif i == 1:
-            boy += 1
-    if girl >= 1:
-        room += 1
-    if boy >= 1:
-        room += 1
+        girl += 1 if i == 0 else 0
+        boy += 1 if i == 1 else 0
+
+    room += 1 if girl >= 1 else 0
+    room += 1 if boy >= 1 else 0
 print(room)
