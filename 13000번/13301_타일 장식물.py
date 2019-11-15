969b7e1e29d3bfb90a10287274aba75ae0880785
zeezlelove <zeezlelove@gmail.com>
2019-11-15 23:33:18 +0900

    백준 문제풀이

--- 13000번/13301_타일 장식물.py
+++ 13000번/13301_타일 장식물.py
@@ -3,7 +3,7 @@ if n == 1:
     print(4)
     exit()
 dp = [0 for i in range(n)]
-dp[0],dp[1] = 1,1
-for i in range(2,n):
-    dp[i] = dp[i-1]+dp[i-2]
-print((max(dp)+(max(dp)+dp[-2]))*2)
+dp[0], dp[1] = 1, 1
+for i in range(2, n):
+    dp[i] = dp[i - 1] + dp[i - 2]
+print((max(dp) + (max(dp) + dp[-2])) * 2)
