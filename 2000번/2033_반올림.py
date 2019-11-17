202fa277d3cbe565781a0f4300e788e975dae027
zeezlelove <zeezlelove@gmail.com>
2019-11-17 23:08:18 +0900

    백준 문제풀이

--- 2000번/2033_반올림.py
+++ 2000번/2033_반올림.py
@@ -1,7 +1,12 @@
-n = int(input())
-t = 1
-while n > t*10:
-    n += 5*t
-    t *= 10
-    n -= n%t
-print(n)
+n = list(map(int,input()))[::-1]
+for i in range(len(n)):
+    if i != len(n)-1:
+        if n[i] >= 5:
+            n[i] = 0
+            n[i+1] +=1
+    if n[i] < 5 and i != len(n)-1:
+        n[i] = 0
+s = ""
+for i in n[::-1]:
+    s += str(i)
+print(int(s))
