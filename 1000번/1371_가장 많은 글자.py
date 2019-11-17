8273b2a22f3b21774d60b76c5cdfc5df4ed99a60
zeezlelove <zeezlelove@gmail.com>
2019-11-17 22:16:59 +0900

    백준 문제풀이

---
+++ 1000번/1371_가장 많은 글자.py
@@ -0,0 +1,16 @@
+dic = {"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0}
+while 1:
+    try:
+        s = list(input())
+        for i in s:
+            if i == " ":continue
+            dic[i] += 1
+    except:
+        break
+
+ans = []
+m = max(dic.values())
+for k,v in dic.items():
+    if v == m:
+        ans.append(k)
+print(*sorted(ans),sep='')
