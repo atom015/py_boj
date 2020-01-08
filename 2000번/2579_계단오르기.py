5000fd1dd65a3749bb030616b7789366f48bac7e
zeezlelove <zeezlelove@gmail.com>
2020-01-08 15:52:02 +0900

    백준 문제풍리

--- 2000번/2579_계단오르기.py
+++ 2000번/2579_계단오르기.py
@@ -4,6 +4,6 @@ dp = [0 for i in range(n)]
 dp[0] = a[0]
 dp[1] = a[0]+a[1]
 dp[2] = max(a[0]+a[2],a[1]+a[2])
-for i in range(2,n):
-    dp[i] = max(dp[i-2]+a[i],a[i-1]+a[i]+dp[i-3])
+for i in range(3,n):
+    dp[i] = max(dp[i-2]+a[i],dp[i-3]+a[i]+a[i-1])
 print(dp[n-1])
