4051b88ca595d045ba91c90ca190329441c5317f
zeezlelove <zeezlelove@gmail.com>
2020-01-23 20:57:55 +0900

    백준 문제풀이

---
+++ 17000번/17609_회문.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(100000)
+def is_palindrome(s):
+    return s == s[::-1]
+def can_palindrome(l,r,chk):
+    if l > r:
+        return True
+    if s[l] == s[r]:
+        return can_palindrome(l+1,r-1,chk)
+    elif chk:
+        return max(can_palindrome(l+1,r,False),can_palindrome(l,r-1,False))
+    else:
+        return False
+def func(s):
+    if is_palindrome(s):
+        return 0
+    elif can_palindrome(0,len(s)-1,1):
+        return 1
+    else:
+        return 2
+
+for i in range(int(input())):
+    s = input()
+    print(func(s))
