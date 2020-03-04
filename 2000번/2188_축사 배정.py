bcfbcaa397da4185a264b8063103e84fb2a2da57
zeezlelove <zeezlelove@gmail.com>
2020-03-04 16:04:05 +0900

    백준 문제풀이

---
+++ 2000번/2188_축사 배정.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(99999999)
+n,m = map(int,input().split())
+arr = [[] for i in range(m+1)]
+d = [0 for i in range(m+1)]
+c = [False for i in range(m+1)]
+
+def dfs(nd):
+    for i in arr[nd]:
+        if c[i]:continue
+        c[i] = True
+
+        if d[i] == 0 or dfs(d[i]):
+            d[i] = nd
+            return True
+    return False
+
+for i in range(1,n+1):
+    li = list(map(int,input().split()))
+    k = li[0]
+    for j in range(1,len(li)):
+        arr[i].append(li[j])
+cnt = 0
+for i in range(1,m+1):
+    c = [False for i in range(m+1)]
+    if dfs(i):cnt += 1
+print(cnt)
