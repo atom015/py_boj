ca91cfca0f20890e73eae4d30e303246e1fdffb3
zeezlelove <zeezlelove@gmail.com>
2020-03-10 00:32:00 +0900

    백준 문제풀이

--- 2000번/2636_치즈.py
+++ 2000번/2636_치즈.py
@@ -1,46 +1,57 @@
 n,m = map(int,input().split())
 arr = [list(map(int,input().split())) for i in range(n)]
+ans = 0
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 1:
             arr[i][j] = 2
-
 def check(x,y):
     cnt = 0
-    for dx,dy in (0,1),(1,0),(-1,0),(0,-1):
-        nx,ny = dx+x,dy+y
-        if 0 <= nx < n and 0 <= ny < m:
-            if arr[nx][ny] == 1:
-                cnt += 1
-    if cnt >= 2:
-        return False
-    return True
-def air():
-    v = [[False for i in range(m)] for i in range(n)]
-    v[0][0] = True
-    arr[0][0] = 1
-    q = [[0,0]]
+    q = [[x,y]]
     while q:
         x,y = q.pop(0)
-        for dx,dy in (0,1),(1,0),(-1,0),(0,-1):
+        for dx,dy in (0,1),(0,-1),(1,0),(-1,0):
+            nx,ny = dx+x,dy+y
+            if 0 <= nx < n and 0 <= ny < m:
+                if arr[nx][ny] == 1:
+                    cnt += 1
+    if cnt >= 1:
+        return False
+    return True
+def cheeze_cnt():
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 2:
+                cnt += 1
+    return cnt
+def bfs1():
+    v = [[False for i in range(m)] for i in range(n)]
+    q = [[0,0]]
+    arr[0][0] = 1
+    v[0][0] = True
+    while q:
+        x,y = q.pop(0)
+        for dx,dy in (0,1),(0,-1),(1,0),(-1,0):
             nx,ny = dx+x,dy+y
             if 0 <= nx < n and 0 <= ny < m:
                 if v[nx][ny] == False:
                     if arr[nx][ny] == 1 or arr[nx][ny] == 0:
                         q.append([nx,ny])
-                        arr[nx][ny] = 1
                         v[nx][ny] = True
-air()
-def all_check():
-    for i in arr:
-        for j in i:
-            if j == 2:
+                        arr[nx][ny] = 1
+bfs1()
+def all_check(arr):
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] != 1:
                 return False
     return True
 def func():
+    global ans
     cnt = 0
-    while not all_check():
-        air()
+    while all_check(arr) == False:
+        ans = cheeze_cnt()
         q = []
         for i in range(n):
             for j in range(m):
@@ -49,6 +60,9 @@ def func():
         while q:
             x,y = q.pop(0)
             arr[x][y] = 1
+        bfs1()
         cnt += 1
     return cnt
-print(func())
+ret = func()
+print(ret)
+print(ans)
