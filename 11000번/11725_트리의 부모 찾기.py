cca45e06f36c217131a9aca9d8012ec47f87852d
zeezlelove <zeezlelove@gmail.com>
2020-03-13 17:12:59 +0900

    백준 문제풀이

--- 11000번/11725_트리의 부모 찾기.py
+++ 11000번/11725_트리의 부모 찾기.py
@@ -1,19 +1,20 @@
-n = int(input())
-arr = [[] for i in range(n+1)]
-for i in range(n-1):
-    a,b = map(int,input().split())
-    arr[a].append(b)
-    arr[b].append(a)
-v = [False for i in range(n+1)]
-p = [0 for i in range(n+1)]
-for i in range(len(arr)):
-    arr[i] = sorted(arr[i])
-def dfs(nd,n):
+from sys import *
+setrecursionlimit(50000000)
+ip = stdin.readline
+def dfs(nd):
     v[nd] = True
-    for i in range(len(arr[nd])):
-        if v[arr[nd][i]] == False:
-            p[arr[nd][i]] = nd
-            dfs(arr[nd][i],n)
-dfs(1,n)
-for i in range(2,n+1):
-    print(p[i])
+    for i in arr[nd]:
+        if v[i] == False:
+            ans[i] = nd
+            dfs(i)
+n = int(ip())
+ans = [0 for i in range(n)]
+arr = [[] for i in range(n)]
+v = [False for i in range(n)]
+for i in range(n-1):
+    a,b = map(int,ip().split())
+    arr[a-1].append(b-1)
+    arr[b-1].append(a-1)
+dfs(0)
+for i in range(1,n):
+    print(ans[i]+1)
