4842ed28ee681db71cf07801e2cb587d379c5a58
zeezlelove <zeezlelove@gmail.com>
2020-03-28 18:10:07 +0900

    백준 문제풀이

---
+++ 1000번/1517_버블 소트.py
@@ -0,0 +1,33 @@
+from sys import *
+setrecursionlimit(10**9)
+ip = stdin.readline
+n = int(ip())
+arr = list(map(int,ip().split()))
+ret = 0
+def merge(s,mid,e):
+    global ret
+    i,j,k = s,mid+1,s
+    ans = []
+    while i <= mid and j <= e:
+        if arr[i] <= arr[j]:
+            ans.append(arr[i])
+            i += 1
+        else:
+            ans.append(arr[j])
+            ret += (mid - i + 1)
+            j += 1
+    while j <= e:
+        ans.append(arr[j])
+        j += 1
+    while i <= mid:
+        ans.append(arr[i])
+        i += 1
+    arr[s:e+1] = ans
+def mergeSort(s,e):
+   if s < e:
+        mid = (s+e)//2
+        mergeSort(s,mid)
+        mergeSort(mid+1,e)
+        merge(s,mid,e)
+mergeSort(0,n-1)
+print(ret)
