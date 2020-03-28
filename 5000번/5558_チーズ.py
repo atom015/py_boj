84c18bc3554925e3692da67544c882b4a3e97b54
zeezlelove <zeezlelove@gmail.com>
2020-03-29 03:45:28 +0900

    백준 문제풀이

---
+++ 5000번/5558_チーズ.py
@@ -0,0 +1,33 @@
+from collections import deque
+from sys import *
+ip = stdin.readline
+n,m,c = map(int,ip().split())
+arr = [list(ip().strip()) for i in range(n)]
+dx,dy = [0,0,1,-1],[1,-1,0,0]
+chk,ret,mx,my = 1,0,0,0
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == "S":
+            mx,my = i,j
+def bfs(v):
+    global chk,ret,mx,my
+    q = deque([])
+    q.append((mx,my,0))
+    v[mx][my] = True
+    while q:
+        x,y,cnt = q.popleft()
+        for i in range(4):
+            nx,ny = dx[i]+x,dy[i]+y
+            if 0 <= nx < n and 0 <= ny < m and not v[nx][ny] and arr[nx][ny] != "X":
+                if arr[nx][ny] in "123456789" and chk >= int(arr[nx][ny]):
+                    chk += 1
+                    mx,my = nx,ny
+                    arr[nx][ny] = "."
+                    ret += cnt+1
+                    return
+                q.append([nx,ny,cnt+1])
+                v[nx][ny] = True
+for i in range(c):
+    v = [[False]*m for i in range(n)]
+    bfs(v)
+print(ret)
