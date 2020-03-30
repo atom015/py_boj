c9ce64dcddab5431f89323292344f721526ec0f3
zeezlelove <zeezlelove@gmail.com>
2020-03-30 18:55:21 +0900

    백준 문제풀이

--- 5000번/5073_삼각형과 세 변.py
+++ 5000번/5073_삼각형과 세 변.py
@@ -1,8 +1,7 @@
 while 1:
-    a,b,c = map(int,input().split())
+    a,b,c = sorted(map(int,input().split()))
     if a == b == c == 0:break
-    s = sorted([a,b,c])
-    if sum(s[:-1]) <= s[2]:
+    if a+b <= c:
         print("Invalid")
     elif a == b == c:
         print("Equilateral")
