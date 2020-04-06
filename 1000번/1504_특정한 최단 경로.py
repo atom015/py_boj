707776018fd79c0c7b58a38db2d0c0694d563f9d
김승우 <zeezlelove@gmail.com>
2020-04-06 13:34:45 +0900

    백준 문제풀이

--- 1000번/1504_특정한 최단 경로.py
+++ 1000번/1504_특정한 최단 경로.py
@@ -4,42 +4,31 @@ ip = sys.stdin.readline
 v,e = map(int, ip().split())
 
 adj = [[] for i in range (v+1)]
-ans = [1e9]*(v+1)
 for i in range(e):
     a, b, c = map(int,ip().split())
     adj[a].append((b, c))
     adj[b].append((a, c))
 s,e = map(int,input().split())
 
-def dijkstra(s):
+def dijkstra(s,p):
+    d = [1e9]*(v+1)
     q = Q.PriorityQueue()
     q.put((0, s))
-    ans[s] = 0
+    d[s] = 0
     while not q.empty():
         pp = q.get()
         here = pp[1]
         dist = pp[0]
-        if ans[here] < dist:
+        if d[here] < dist:
             continue
         for i in adj[here]:
             cost = dist + i[1]
-            if ans[i[0]] > cost:
-                ans[i[0]] = cost
+            if d[i[0]] > cost:
+                d[i[0]] = cost
                 q.put((cost, i[0]))
-dijkstra(1)
-ans1 = ans[s]
-ans2 = ans[e]
-ans = [1e9]*(v+1)
-if ans1 == 1e9 and ans2 == 1e9:
-    print(-1)
+    return d[p]
+ans = min(dijkstra(1,s)+dijkstra(s,e)+dijkstra(e,v),dijkstra(1,e)+dijkstra(e,s)+dijkstra(s,v))
+if 0 < ans < 1e9:
+    print(ans)
 else:
-    dijkstra(s)
-    ans1 += ans[e]
-    ans2 += ans[e]
-    ans = [1e9]*(v+1)
-    dijkstra(e)
-    ans1 += ans[v]
-    ans = [1e9]*(v+1)
-    dijkstra(s)
-    ans2 += ans[v]
-    print(min(ans1,ans2))
+    print(-1)
