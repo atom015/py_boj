f9c5e24a11f1d009ad446b6da4d6e28e1313aeca
김승우 <zeezlelove@gmail.com>
2020-05-08 18:59:38 +0800

    백준 문제풀이

--- 2000번/2579_계단오르기.py
+++ 2000번/2579_계단오르기.py
@@ -1,6 +1,6 @@
 n = int(input())
-a = [int(input()) for i in range(n)]
-dp = [0 for i in range(n)]
+a = [int(input()) for i in range(n)]+[0]*4
+dp = [0 for i in range(n+4)]
 dp[0] = a[0]
 dp[1] = a[0]+a[1]
 dp[2] = max(a[0]+a[2],a[1]+a[2])
