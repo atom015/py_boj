f9fc5cc0ad80341353973399aa636ea26e810bd2
김승우 <zeezlelove@gmail.com>
2020-07-10 23:40:02 +0900

    백준 문제풀이

--- 17000번/17610_양팔저울.py
+++ 17000번/17610_양팔저울.py
@@ -2,6 +2,7 @@ n = int(input())
 arr = list(map(int,input().split()))
 s = sum(arr)
 ch = [0 for i in range(s+1)]
+
 def dfs(i,w):
     ch[w] = 1
     if i >= n:
@@ -9,5 +10,6 @@ def dfs(i,w):
     dfs(i+1,w)
     dfs(i+1,w+arr[i])
     dfs(i+1,abs(w-arr[i]))
+
 dfs(0,0)
 print(ch.count(0))
