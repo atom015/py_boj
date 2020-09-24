c25376dffd400d7b45c3aaab79e568fa4f588aed
김승우 <zeezlelove@gmail.com>
2020-09-25 00:44:32 +0900

    백준 문제풀이

--- 1000번/1918_후위 표기식.py
+++ 1000번/1918_후위 표기식.py
@@ -1,18 +1,18 @@
 s = input()
 st,a = [],""
 size = {"+":1,"-":1,"/":2,"*":2,"(":0}
-for i in range(len(s)):
-    if "A" <= s[i] <= "Z":
-        print(s[i],end='')
-    elif s[i] == "(":
+for i in s:
+    if "A" <= i <= "Z":
+        print(i,end='')
+    elif i == "(":
         st.append("(")
-    elif s[i] == ")":
+    elif i == ")":
         while st and st[-1] != "(":
             print(st.pop(),end='')
         st.pop()
     else:
-        while st and size[st[-1]] >= size[s[i]]:
+        while st and size[st[-1]] >= size[i]:
             print(st.pop(),end='')
-        st.append(s[i])
+        st.append(i)
 while st:
     print(st.pop(),end='')
